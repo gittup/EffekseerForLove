e0378d0e5142b1083e632e75263fa3a9ddcd0e61
Mike Shal <marfey@gmail.com>
2021-05-05 18:30:00 -0700

    Use LDEXT to specify shared library extension

--- Tupfile.lua
+++ Tupfile.lua
@@ -1,5 +1,5 @@
 if LD then
-	tup.rule('<objs>', '^ LD %o^ $(LD) %<objs> -o %o $(LDFLAGS)', 'effekseer.so')
+	tup.rule('<objs>', '^ LD %o^ $(LD) %<objs> -o %o $(LDFLAGS)', 'effekseer.' .. LDEXT)
 elseif AR then
 	tup.rule('<objs>', '^ AR %o^ $(AR) cr %o %<objs>', 'effekseer.wasm')
 end
