cf93196f54244d5e48977bafe8a6b1fbffe40c12
Mike Shal <marfey@gmail.com>
2021-05-05 18:30:35 -0700

    Remove duplicate headers in effekseer/src/include.
    When there's 2 copies it makes it confusing which one to update or edit.

--- Tuprules.lua
+++ Tuprules.lua
@@ -4,8 +4,8 @@ EFFEKSEER_PLUGIN = tup.getcwd()
 tup.include(tup.getconfig('TUP_PLATFORM') .. '.lua')
 
 CFLAGS += '`pkg-config luajit --cflags`'
-CFLAGS += '-I' .. EFFEKSEER_PLUGIN .. '/src/effekseer/src/include/'
 CFLAGS += '-I' .. EFFEKSEER_PLUGIN .. '/src/effekseer/src/Effekseer/'
+CFLAGS += '-I' .. EFFEKSEER_PLUGIN .. '/src/effekseer/src/EffekseerRendererGL/'
 CFLAGS += '-fPIC'
 CFLAGS += '-g'
 CFLAGS += '-O2'
