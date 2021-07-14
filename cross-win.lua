54a5f25a6340758021b58e93c2cbab9b4ceb20a3
Mike Shal <marfey@gmail.com>
2021-07-14 16:45:06 -0700

    Remove -lws2_32 from cross-win builds.

--- cross-win.lua
+++ cross-win.lua
@@ -4,7 +4,6 @@ CC = 'x86_64-w64-mingw32-g++-posix'
 LD = 'x86_64-w64-mingw32-g++-posix'
 LDFLAGS += '-L' .. luajit_path
 LDFLAGS += '-shared'
-LDFLAGS += '-lws2_32'
 LDFLAGS += '-lglu32'
 LDFLAGS += '-lopengl32'
 LDFLAGS += '-lluajit-5.1'
